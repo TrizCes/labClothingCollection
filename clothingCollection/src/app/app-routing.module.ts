34cf4bcb06395033ccb1c432776201f92e4f5fb3
Beatriz Ceschini <biaceschini@gmail.com>
2023-03-22 15:28:30 -0300

    fix git problems
    I tried to fix it but some problems persist on my branchs

--- clothingCollection/src/app/app-routing.module.ts
+++ clothingCollection/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { FullComponent} from './layouts/full/full.component';
 import { ContentComponent } from './layouts/content/content.component';
 import { NotFoundComponent } from './component/not-found/not-found.component';
+<<<<<<< Updated upstream
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
@@ -12,6 +13,21 @@ const routes: Routes = [
   ]},
   {path: 'login', component: ContentComponent, children:[
 
+=======
+import { LoginComponent } from './pages/login/login.component'
+import { AuthGuard } from './security/auth.guard';
+import { SenhaEsquecidaComponent } from './pages/senhaEsquecida/senha-esquecida/senha-esquecida.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  {path: 'dashboard', component: FullComponent, canActivate: [AuthGuard],
+  children: [
+
+  ]},
+  {path: 'login', component: ContentComponent, children:[
+    {path: '', component: LoginComponent },
+    {path: 'password', component: SenhaEsquecidaComponent}
+>>>>>>> Stashed changes
   ]},
   {path: 'not-found', component: NotFoundComponent},
   {path: '**', redirectTo: 'not-found'}
--- clothingCollection/src/app/app.module.ts
+++ clothingCollection/src/app/app.module.ts
@@ -10,21 +10,42 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FullComponent } from './layouts/full/full.component';
 import { ContentComponent } from './layouts/content/content.component';
 import { NotFoundComponent } from './component/not-found/not-found.component';
+<<<<<<< Updated upstream
 import { LoginComponent } from './pages/login/login.component';
 import { SenhaEsquecidaComponent } from './pages/senhaEsquecida/senha-esquecida/senha-esquecida.component';
 
 
 import { AuthService } from './services/auth.service';
 
+=======
+<<<<<<< Updated upstream
+=======
+import { LoginComponent } from './pages/login/login.component';
+
+import { AuthService } from './services/auth.service';
+import { SenhaEsquecidaComponent } from './pages/senhaEsquecida/senha-esquecida/senha-esquecida.component'
+
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
     AppComponent,
     FullComponent,
     ContentComponent,
+<<<<<<< Updated upstream
     NotFoundComponent,
     LoginComponent,
     SenhaEsquecidaComponent,
+=======
+<<<<<<< Updated upstream
+    NotFoundComponent
+=======
+    NotFoundComponent,
+    LoginComponent,
+    SenhaEsquecidaComponent,
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
